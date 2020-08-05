ecd09c1d3923b30154f4c2fbdb64082e9d8d3a75
Yarik <uwebtechnology@gmail.com>
2020-08-05 17:11:29 +0300

    Remove externals

--- webpack.config.js
+++ webpack.config.js
@@ -36,20 +36,6 @@ module.exports = {
   optimization: {
     minimize: true
   },
-  externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: '_'
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: '_'
-    }
-  },
   plugins: [
     new CopyPlugin({
       patterns: [
